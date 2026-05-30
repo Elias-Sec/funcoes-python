435b3545cd63cfd52214061b560be699c45c5766
Elias-sec <wendell.infosec@gmail.com>
2026-05-30 16:14:58 -0300

    add exe10

---
+++ exe10.py
@@ -0,0 +1,13 @@
+def login(usuario,senha):
+    if usuario == "admin" and senha == "1234":
+        return "Login bem-sucedido!"
+    else:
+        return "Usuário ou senha incorretos."
+
+
+usuario_input = input("Digite o nome de usuário: ")
+senha_input = input("Digite a senha: ")
+
+login(usuario_input, senha_input)
+print(login(usuario_input, senha_input))
+
