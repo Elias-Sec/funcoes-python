239de963204b4b9b1a9174493277d2f03b43d757
Elias-sec <wendell.infosec@gmail.com>
2026-05-30 15:54:28 -0300

    add exe08

---
+++ exe08.py
@@ -0,0 +1,13 @@
+def media_lista(numeros):
+    if len(numeros) == 0:
+        return 0
+    else:
+        return sum(numeros) / len(numeros)
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+numeros = [num1, num2, num3]
+media = media_lista(numeros)
+print("A média dos números é:", media)
